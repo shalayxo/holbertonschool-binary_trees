21fac60bdf4d22ba2ebaf92c8883b0e7865ea4ac
shalayxo <shalaywilson3695@gmail.com>
2023-08-23 15:40:20 -0700

    delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer to root node of tree to delete
+ * Return: nothing
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+if (tree != NULL)
+{
+binary_tree_delete(tree->left);
+binary_tree_delete(tree->right);
+free(tree);
+}
+}
